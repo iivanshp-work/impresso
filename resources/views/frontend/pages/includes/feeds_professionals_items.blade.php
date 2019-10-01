4348c854ab824288028edbb7803db283c619f0f0
Ivan Shpilyarevych <ivan@npgroup.net>
2019-10-01 09:25:30 +0300

    New changes - connected button

--- resources/views/frontend/pages/includes/feeds_professionals_items.blade.php
+++ resources/views/frontend/pages/includes/feeds_professionals_items.blade.php
@@ -77,7 +77,11 @@
             @if (!$meetup || ($meetup && $meetup->status == 3) || ($meetup && $meetup->status == 4)) {{--failed-declined meetup--}}
                 <a href="{{url('/profile/' . $professional->id . '/meetup')}}" class="btn btn-violet btn-meetup">Meetup</a>
             @elseif ($meetup && $meetup->status == 2)
-                <button type="button" class="btn btn-violet btn-meetup-connected">Connected</button>
+                @if ($user->id == $meetup->user_id_invited)
+                    <a href="@if ($meetup->invitingUser && $meetup->invitingUser->phone)tel:{{$meetup->invitingUser->phone}}@else{{"#"}}@endif" class="btn btn-violet btn-meetup-connected">Call to Meetup</a>
+                @else
+                    <a href="@if ($meetup->invitedUser && $meetup->invitedUser->phone)tel:{{$meetup->invitedUser->phone}}@else{{"#"}}@endif" class="btn btn-violet btn-meetup-connected">Call to Meetup</a>
+                @endif
             @elseif ($meetup && $meetup->status == 1 && $meetup->user_id_inviting == $user->id)
                 <button type="button" class="btn btn-gray btn-meetup-send">Meetup invite sent</button>
             @elseif ($meetup && $meetup->status == 1 && $meetup->user_id_invited == $user->id)
