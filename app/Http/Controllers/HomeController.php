695823e4484fa2026b621a9e705b0284bcc4eb89
Ivan Shpilyarevych <ivan@npgroup.net>
2019-11-21 07:52:06 +0200

    Maintenance mode.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,6 +6,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\LA\UploadsController;
 use App\Http\Requests;
 use App\Models\Upload;
 use App\User;
@@ -34,26 +35,43 @@ class HomeController extends Controller
     public function index() {
 
         //uploads update path
-        $uploads = Upload::all();
+        /*$uploads = Upload::all();
         if ($uploads) {
             foreach ($uploads as $upload) {
                 $upload->path = str_replace('/home/xobaedic/www/valididentity.ch', $_SERVER['DOCUMENT_ROOT'], $upload->path);
-                test($upload->path);
+                //test($upload->path);
                 $upload->save();
             }
         }
         //users update images for facebook and linkedin providers
-        test("Exit");
+        test("Exit");*/
+        /*
         $users = User::where('provider', 'facebook')->get();
         if ($users) {
             foreach($users as $user) {
+                if ($user->photo && $user->photo > 1990) continue;
+                if ($user->photo){
+                    Upload::where('id', $user->photo)->delete();
+                }
                 $photo = 0;
                 $fbId = $user->provider_id;
+                $skipFBIDS = [
+                    '2527231397552964',
+                ];
+                if (in_array($fbId, $skipFBIDS)) continue;
                 if ($fbId) {
                     $image = 'https://graph.facebook.com/v2.9/' . $fbId . '/picture?type=normal';
+                    //if ($data = @file_get_contents($image)) {
+                    //    $data = json_decode($data);
+                    //    if (json_last_error() == JSON_ERROR_NONE) {
+                    //        test($data);
+                    //        continue;
+                    //    }
+                    //}
 
                     try {
-                        $image = str_replace('?type=normal', 'jpg', $image);
+                        //$image = str_replace('?type=normal', 'jpg', $image);
+
                         $uploadController = new UploadsController;
                         $responseImage = $uploadController->upload_files(true, $image, ["width" => 200, "height" => 200], true);
                         if ($responseImage["status"] == "success") {
@@ -67,12 +85,13 @@ class HomeController extends Controller
                 $user->save();
             }
         }
-        test($users);
+        test("Done");
+        test($users);*/
         //manual for linkedin
         return view('home');
     }
 
-    /**
+    /*
      * maintenance mode page
      *
      * @return Response
